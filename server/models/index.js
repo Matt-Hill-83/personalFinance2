87461262ef9b74b8591e22c04033a714ff4e87f7
KletusVandamme <matt.hill.email@gmail.com>
2017-04-03 01:00:09 -0700

    works

--- server/models/index.js
+++ server/models/index.js
@@ -14,6 +14,8 @@ var config    = require(__dirname + '/../config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
+	console.log('using_env_variable');
+	
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
