ed5a2e2e217e1e54bad79d9a0083795329e4bf4c
KletusVandamme <matt.hill.email@gmail.com>
2017-04-13 04:09:25 -0700

    works

--- client/js/constants.service.js
+++ client/js/constants.service.js
@@ -3,7 +3,8 @@ angular.module('app').factory('Constants', [ 'Utilities', Constants_]);
 
 function Constants_(Utilities) {
   var tableConfig = {
-    numColInTable       : 17,
+    numColInTable       : 2,
+    // numColInTable       : 17,
     startDate           : '01/01/2017',
     endDate             : null,
     dates               : [],
